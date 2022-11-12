cd413c91320e351a551c6679487cc7aefeaed38f
Surya742 <suryakumarsahani.742@gmail.com>
2022-11-12 20:31:49 +0530

    fixed typos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ function Footer() {
   return (
     <div className="footer-head">
       <p className="footer-headdesc">Making women healthcare services available around the world</p>
+      <p className="footerdesc">No data is getting collected. Feel free to chat</p>
     </div>
   );
 }
