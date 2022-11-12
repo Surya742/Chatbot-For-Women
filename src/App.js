f0e3edd85c4711510f32ce75f2bfe7b120398a89
Surya742 <suryakumarsahani.742@gmail.com>
2022-11-12 16:18:54 +0530

    implemented chating

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import Chatbot from 'react-chatbot-kit';
+import './components/chatcss.css';
 import './App.css';
+
+import ActionProvider from './components/ActionProvider';
+import MessageParser from './components/MessageParser';
+import config from './components/config';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Chatbot className="chats" config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
       </header>
     </div>
   );
